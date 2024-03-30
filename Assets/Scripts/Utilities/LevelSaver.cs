8d038937787e761c8cd952a76625523b96d3a988
Mert ERGÜN <mertergun17@hotmail.com>
2024-03-30 15:55:06 +0300

    Level saving method updated

--- Assets/Scripts/Utilities/LevelSaver.cs
+++ Assets/Scripts/Utilities/LevelSaver.cs
@@ -1,22 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSaver : MonoBehaviour
 {
-    public int level = 1;
+    public int level;
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey("level"))
+        // Get scene name
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        // If scene name is MainMenu
+        if (sceneName == "MainMenu")
         {
-            level = PlayerPrefs.GetInt("level");
+            if (PlayerPrefs.HasKey("level"))
+            {
+                if (PlayerPrefs.GetInt("level") == level)
+                {
+                    return;
+                } else
+                {
+                    SaveLevel();
+                }
+            } else if (level != 1)
+            {
+                SaveLevel();
+            } else
+            {
+                ResetLevel();
+            }
         }
+        else if (sceneName == "Gameplay")
+        {
+            if (IsStartedFromMainMenu())
+            {
+                SetStartedFromMainMenu(false);
+                LoadLevel();
+            } else
+            {
+                SaveLevel();
+            }  
+        } 
     }
 
     public void SaveLevel()
     {
         PlayerPrefs.SetInt("level", level);
+    }
+
+    private bool IsStartedFromMainMenu()
+    {
+        return PlayerPrefs.GetInt("fromMainMenu") == 1;
+    }
+
+    public void SetStartedFromMainMenu(bool value)
+    {
+        PlayerPrefs.SetInt("fromMainMenu", value ? 1 : 0);
     }
 
     public void LoadLevel()
